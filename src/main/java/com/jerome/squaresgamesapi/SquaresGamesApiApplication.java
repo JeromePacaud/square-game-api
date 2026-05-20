3bb20351fc3bf3a3fa87acb0a8e99fc18ea482df
Jerome <jeromepacaud39@gmail.com>
2026-05-20 13:48:56 +0200

    Set up Spring Boot project

---
+++ src/main/java/com/jerome/squaresgamesapi/SquaresGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jerome.squaresgamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquaresGamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquaresGamesApiApplication.class, args);
+    }
+
+}
